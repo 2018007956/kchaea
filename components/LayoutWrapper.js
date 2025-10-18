2b1ee84df05804fdbf664faad889f977a630ada5
kchaea <chaea11s0@gmail.com>
2025-10-18 12:03:26 +0900

    fix: removed the footer to fix the issue where it appeared in the middle of blog posts

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -64,7 +64,6 @@ const LayoutWrapper = ({ children }) => {
           </div>
         </header>
         <main className="mb-auto">{children}</main>
-        <Footer />
       </div>
     </SectionContainer>
   )
