fd1b45b6912620f294c9f9a88975739e3b09a245
2018007956 <chaea11s0@gmail.com>
2026-01-02 02:19:23 +0900

    docs: add blog post

--- data/quotesData.js
+++ data/quotesData.js
@@ -1,7 +1,15 @@
 export const quotes = [
   {
+    author: '니체',
+    quote: `명랑하게 살아라. 인간은 다 죽는다. 고민과 걱정으로 시간을 낭비하지 마라. 인생을 즐기지 못하는 것이야말로 가장 어리석은 일이다.`,
+  },
+  {
+    author: '송광사 연성스님',
+    quote: `더 높은 가치가 있다는 것을 아니까, 장난감이 부서져도 아무렇지도 않다.`,
+  },
+  {
     author: 'Kendall Jenner, Calvin Klein Interview',
-    quote: `I don't chase, I attract.\nWhat's meant for me will simply find me.`,
+    quote: `I don't chase, I attract. What's meant for me will simply find me.`,
   },
   {
     author: '노희영',
