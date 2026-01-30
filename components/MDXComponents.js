e5d35249a400230895527bc17dc48ea35f6ed409
2018007956 <chaea11s0@gmail.com>
2026-01-30 21:38:10 +0900

    fix: dynamic require in MDX layout loading caused 500 err - MDX 에서 가 로 로드되는 케이스 제거 - 를 정적 import + 레이아웃 매핑() 방식으로 변경해 안정적으로 렌더링되도록 수정

--- components/MDXComponents.js
+++ components/MDXComponents.js
@@ -6,6 +6,21 @@ import CustomLink from './Link'
 import TOCInline from './TOCInline'
 import Pre from './Pre'
 import { BlogNewsletterForm } from './NewsletterForm'
+import PostLayout from '../layouts/PostLayout'
+import PostSimple from '../layouts/PostSimple'
+import AuthorLayout from '../layouts/AuthorLayout'
+import ActivityLayout from '../layouts/ActivityLayout'
+import ListLayout from '../layouts/ListLayout'
+import SnippetsLayout from '../layouts/SnippetsLayout'
+
+const Layouts = {
+  PostLayout,
+  PostSimple,
+  AuthorLayout,
+  ActivityLayout,
+  ListLayout,
+  SnippetsLayout,
+}
 
 export const MDXComponents = {
   Image,
@@ -14,7 +29,11 @@ export const MDXComponents = {
   pre: Pre,
   BlogNewsletterForm: BlogNewsletterForm,
   wrapper: ({ components, layout, ...rest }) => {
-    const Layout = require(`../layouts/${layout}`).default
+    const Layout = Layouts?.[layout]
+    if (!Layout) {
+      const available = Object.keys(Layouts).join(', ')
+      throw new Error(`Invalid MDX layout "${layout}". Available layouts: ${available}`)
+    }
     return <Layout {...rest} />
   },
 }
